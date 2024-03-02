82bcd4547e3be5293d2a8625b53cb9d8d35af28d
leonardo rodrigues <leonardorodrigues_f@hotmail.com>
2024-03-02 19:02:57 -0300

    trocando a instância

--- infra/main.tf
+++ infra/main.tf
@@ -12,19 +12,19 @@ resource "aws_rds_cluster" "postgres_cluster" {
   engine                    = "postgres"
   engine_version            = "16"
   master_username           = "jamal"
-  master_password           = "jamal69guloso"
+  master_password           = "jamal@aws666"
   db_subnet_group_name      = aws_db_subnet_group.rds_subnet_group.name
   skip_final_snapshot       = true
   allocated_storage         = 2
   storage_type              = "gp2"
-  db_cluster_instance_class = "db.t3.micro"
+  db_cluster_instance_class = "db.t3.small"
   availability_zones = [ "us-east-1a" ]
 }
 
-# resource "aws_rds_cluster_instance" "postgres_instance" {
-#   count                     = 1
-#   cluster_identifier        = aws_rds_cluster.postgres_cluster.id
-#   instance_class            = "db.t3.micro"
-#   engine                    = "postgres"
-#   engine_version            = "16.2"
-# }
+resource "aws_rds_cluster_instance" "postgres_instance" {
+  count                     = 1
+  cluster_identifier        = aws_rds_cluster.postgres_cluster.id
+  instance_class            = "db.t3.small"
+  engine                    = "postgres"
+  engine_version            = "16.2"
+}
