59d201851d6068032cd19495eaef4db01c6b94d5
leonardo rodrigues <leonardorodrigues_f@hotmail.com>
2024-03-02 17:54:33 -0300

    ajuste instancia

--- infra/main.tf
+++ infra/main.tf
@@ -17,14 +17,14 @@ resource "aws_rds_cluster" "postgres_cluster" {
   skip_final_snapshot       = true
   allocated_storage         = 2
   storage_type              = "gp2"
-  db_cluster_instance_class = "db.t2.micro"
+  db_cluster_instance_class = "db.t3.micro"
   availability_zones = [ "us-east-1a" ]
 }
 
 # resource "aws_rds_cluster_instance" "postgres_instance" {
 #   count                     = 1
 #   cluster_identifier        = aws_rds_cluster.postgres_cluster.id
-#   instance_class            = "db.t2.micro"
+#   instance_class            = "db.t3.micro"
 #   engine                    = "postgres"
 #   engine_version            = "16.2"
 # }
