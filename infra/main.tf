615da55be4aa604e1f1cc63ed5e871c81eb43af7
leonardo rodrigues <leonardorodrigues_f@hotmail.com>
2024-03-19 01:11:29 -0300

    ajustes

--- infra/main.tf
+++ infra/main.tf
@@ -1,19 +1,20 @@
 provider "aws" {
   region = "sa-east-1"
 }
+
 resource "aws_db_instance" "fiap-lanches-postgres-db-instance" {
-  allocated_storage   = 20
-  engine              = "postgres"
-  engine_version      = "16"
-  identifier          = "fiap-lanches-postgres-db-instance"
-  instance_class      = "db.t3.micro"
-  password            = "mysecretpassword"
-  skip_final_snapshot = true
-  storage_encrypted   = false
-  publicly_accessible = true
-  username            = "postgres"
-  apply_immediately   = true
-  db_subnet_group_name = "fiaplanches"
+  allocated_storage      = 20
+  engine                 = "postgres"
+  engine_version         = "16"
+  identifier             = "fiap-lanches-postgres-db-instance"
+  instance_class         = "db.t3.micro"
+  password               = "mysecretpassword"
+  skip_final_snapshot    = true
+  storage_encrypted      = false
+  publicly_accessible    = true
+  username               = "postgres"
+  apply_immediately      = true
+  db_subnet_group_name   = "fiaplanches"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
 }
 
@@ -35,10 +36,10 @@ resource "aws_security_group" "allow_tls" {
 }
 
 resource "aws_security_group_rule" "allow_all" {
-  type              = "ingress"
-  to_port           = 5432
-  protocol          = "tcp"
-  from_port         = 5432
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_blocks = ["0.0.0.0/0"]
+  type                = "ingress"
+  to_port             = 5432
+  protocol            = "tcp"
+  from_port           = 5432
+  security_group_id   = aws_security_group.allow_tls.id
+  cidr_blocks         = ["0.0.0.0/0"]
 }
